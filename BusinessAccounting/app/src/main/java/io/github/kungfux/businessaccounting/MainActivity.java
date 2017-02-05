e704f80bdcace270e8816fb8261e3c2d05e0aeaf
Alexander Fuks <kungfux2010@gmail.com>
2017-02-05 23:45:59 +0300

    Added starting of another activity

--- BusinessAccounting/app/src/main/java/io/github/kungfux/businessaccounting/MainActivity.java
+++ BusinessAccounting/app/src/main/java/io/github/kungfux/businessaccounting/MainActivity.java
@@ -10,9 +10,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -53,40 +53,24 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }*/
-
-    /*@Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        Intent intent = null;
+
         switch(id) {
-            //case R.id.nav_transactions:
+            case R.id.nav_transactions:
+                intent = new Intent(this, MainActivity.class);
+                break;
             default:
                 Toast toast = Toast.makeText(this, R.string.not_implemented, Toast.LENGTH_LONG);
                 toast.show();
                 break;
+        }
+
+        if (intent != null) {
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
